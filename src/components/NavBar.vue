06099d7d066b58a346dce481008c6c9cbe65bbf4
Hari00155 <ahariharananandan@gmail.com>
2026-01-29 19:35:25 +0530

    nav bar update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -46,12 +46,7 @@
       </div>
     </div>
   </nav> 
-  <footer>
-  <div class="container text-center">
-    <p class="mb-1">Lucas TVS Proving & Reliability Lab</p>
-    <small>Ensuring Quality & Reliability</small>
-  </div>
-</footer>
+
 </template>
 <script setup>
 // No script logic needed for this basic example.
